b246f01ff21d9ad124528993c3a66a3401066fea
Amina Ly <amina.ly626@gmail.com>
2019-08-29 16:57:07 -0700

    small sh changes

---
+++ precip_extract.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=calc_precips2
+#SBATCH --nodes=1
+#SBATCH --array=1-1
+#SBATCH --time=05:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=60GB
+#SBATCH --mail-type=END,FAIL 
+#SBATCH --mail-user=aminaly@stanford.edu
+#SBATCH -p diffenbaugh
+
+
+module load R/3.4.0
+
+cd $OAK/group_members/aminaly/precip-price
+let buffer=$SLURM_ARRAY_TASK_ID
+Rscript ./calc_precips.R $buffer 
