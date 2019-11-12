8048274dc00d86e586656ce4588f80322e8b9ab6
Amina Ly <aminaly@SE3-C02W3433HV29.local>
2019-11-11 18:37:34 -0800

    ndvi stack updates

--- ndvi_stack.R
+++ ndvi_stack.R
@@ -20,6 +20,9 @@ ifelse(dir.exists("/Users/amina/Documents/Stanford/precip-price"),
 ## read in the cropland tif file as raster
 #croplands <- raster("../mosaic_cropland.tif", RAT = T)
 
+#get price data
+price <- readRDS("saved-output/formatted-price.rds")
+
 ## pick up args from commandline/sbatch
 years <- c(2007:2017)
 args <- commandArgs(trailingOnly = TRUE)
@@ -32,11 +35,20 @@ num_files <- length(ndvi_files)
 
 #Filter out only those that are croplands (=2) and turn into a bunch of points
 #crop_points <- rasterToPoints(croplands, fun=function(x){x==2}, spatial=T)
-#saveRDS(crop_points, paste0(getwd(),"/saved-output/crop_as_points.rds"))
-crop_points <- readRDS("~/saved-output/crop_as_points.rds")
+#saveRDS(crop_points, "../crop_as_points.rds")
+crop_points <- readRDS("../crop_as_points.rds")
 
-#Extract over points and get NDVI data
-# Run through precipitation brick and extract over the buffers
+#Extract over points and get NDVI data only in croplands and then collective NDVI in market area
+
+#get market locations 
+locs <- unique(price[,c(1:2,16:17)])
+locs <- locs[which(locs$latitude < 999),]
+
+# Transform your markets
+markets = st_as_sf(locs,coords=c("longitude","latitude"))
+st_crs(markets) <- 4326
+markets <- st_transform(markets, 4326)
+
 ndvi_data <- c()
 
 for(i in 1:numfiles){
@@ -45,11 +57,13 @@ for(i in 1:numfiles){
   nd <- raster(ndvi_files[i]) %>% crop(extent(croplands))
   
   temp <- c()
-  temp$coords <- coordinates(crop_points)
+  coords <- coordinates(crop_points)
+  temp$lon <- coords$x
+  temp$lat <- coords$y
   temp$date <- rep(getZ(nd), nrow(temp$coords))
 
-  velox_obj <- velox(nd)
-  temp_ndvi_by_point <- velox_obj$extract_points(sp = crop_points)
+  velox_obj <- velox(crop_points)
+  temp_ndvi_by_point <- velox_obj$extract(sp = markets, small = T)
   
   temp$temp_mean <- lapply(temp_ndvi_by_point, function(x){mean(x, na.rm = T)}) %>% unlist()  
   
@@ -57,7 +71,20 @@ for(i in 1:numfiles){
 }
 
 #save this out to make my life easier
-saveRDS(ndvi_data, paste0(getwd(), paste0("/saved-output/ndvi_croplands_", year,".rds"))
+saveRDS(ndvi_data, paste0(getwd(), "/saved-output/ndvi_croplands_", year,".rds"))
 
+#Now run through each location and find the inflection points. We want a table of:
+# lat, lon, start month, end month
+
+coords <- coordinates(crop_points)
+for(i in nrow(coords)) {
+  
+  temp_coord <- coords[i,]
+  temp <- ndvi_data %>% filter(lon == temp_coord[1]) %>% filter(lat == temp_coord[2])
+  
+  
+  
+  
+}
 
 
