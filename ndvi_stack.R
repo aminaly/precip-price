2c92e614518702366297a948129da3e642ec8fd4
Amina Ly <aminaly@SE3-C02W3433HV29.local>
2019-11-12 17:54:23 -0800

    updates to ndvi stack

--- ndvi_stack.R
+++ ndvi_stack.R
@@ -38,8 +38,6 @@ num_files <- length(ndvi_files)
 #saveRDS(crop_points, "../crop_as_points.rds")
 crop_points <- readRDS("../crop_as_points.rds")
 
-#Extract over points and get NDVI data only in croplands and then collective NDVI in market area
-
 #get market locations 
 locs <- unique(price[,c(1:2,16:17)])
 locs <- locs[which(locs$latitude < 999),]
@@ -49,26 +47,55 @@ markets = st_as_sf(locs,coords=c("longitude","latitude"))
 st_crs(markets) <- 4326
 markets <- st_transform(markets, 4326)
 
+#Extract over points and get NDVI data only in croplands and then collective NDVI in market area
 ndvi_data <- c()
+bufs <- c(.25, .5, .75, 1, 2, 3, 4, 5)
 
-for(i in 1:numfiles){
-  print(i)
+#first extract over NDVI in the markets (avg NDVI of croplands within x miles)
+for(buf in bufs) {
   
-  nd <- raster(ndvi_files[i]) %>% crop(extent(croplands))
+  #check to see if we've extracted before
+  rdsname <- paste0("ndvi/", buf, "_ndvi.rds")
+  #if(file.exists(paste0(getwd(), rdsname))) stop("Already exists")
   
-  temp <- c()
-  coords <- coordinates(crop_points)
-  temp$lon <- coords$x
-  temp$lat <- coords$y
-  temp$date <- rep(getZ(nd), nrow(temp$coords))
-
-  velox_obj <- velox(crop_points)
-  temp_ndvi_by_point <- velox_obj$extract(sp = markets, small = T)
+  # set up the buffers for your markets
+  markets_buffer = st_buffer(markets, buf)
+  markets_buffer <- as(markets_buffer, 'Spatial')
   
-  temp$temp_mean <- lapply(temp_ndvi_by_point, function(x){mean(x, na.rm = T)}) %>% unlist()  
+  #first, lets crop the croplands by the markets to reduce the number of points we're dealing with
+  cropped_crops <- raster::crop(croplands, markets_buffer)
+  cropped_crops <- mask(cropped_crops, markets_buffer)
+  #cropped_crops <- trim(cropped_crops, values = NA)
   
-  ndvi_data <- bind_rows(ndvi_data, temp)
+  # Run through  ndvi files and extract over the cropland buffers
+  for(i in 1:numfiles){
+    print(i)
+    
+    nd <- raster(ndvi_files[i]) %>% crop(extent(croplands))
+    
+    temp <- c()
+    coords <- coordinates(crop_points)
+    temp$lon <- coords$x
+    temp$lat <- coords$y
+    temp$date <- rep(getZ(nd), nrow(temp$coords))
+    
+    velox_obj <- velox(crop_points)
+    temp_ndvi_by_point <- velox_obj$extract(sp = cropped_crops, small = T)
+    
+    temp$temp_mean <- lapply(temp_ndvi_by_point, function(x){mean(x, na.rm = T)}) %>% unlist()  
+    
+    ndvi_data <- bind_rows(ndvi_data, temp)
+  }
+  
+  #save this out to make my life easier
+  saveRDS(all_data, paste0(getwd(), "/", rdsname))
+  precip <- readRDS(rdsname)
+  
 }
+
+
+
+
 
 #save this out to make my life easier
 saveRDS(ndvi_data, paste0(getwd(), "/saved-output/ndvi_croplands_", year,".rds"))
